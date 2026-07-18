e6ae77b1e46009bd27923869cd2281abb1753248
charly <nandry556@gmail.com>
2026-07-18 14:29:24 +0200

    fix: update sitemap URL in robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://eloi-charly01.github.io/portfolio/sitemap.xml',
+    sitemap: 'https://eloi-charly01.github.io/sitemap.xml',
   }
 }
